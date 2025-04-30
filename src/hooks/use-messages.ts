ad43515d2bfb7a79a2b4e40410d4d926a03cda16
Achmad Fathullah <fathullah@qiscus.co>
2025-04-30 10:58:19 +0700

    fix(hooks): simplify type annotation for messages_ and ensure roomId is used directly in useMessages

--- src/hooks/use-messages.ts
+++ src/hooks/use-messages.ts
@@ -3,11 +3,11 @@ import { messagesAtom, roomIdAtom } from '../state';
 import { useAtomValue } from 'jotai';
 
 export function useMessages() {
-  const messages_: Record<string, { chatRoomId: string; timestamp: Date }> = useAtomValue(messagesAtom);
+  const messages_ = useAtomValue(messagesAtom);
   const roomId = useAtomValue(roomIdAtom);
   const messages = useMemo(() => {
     return Object.values(messages_)
-      .filter((it) => it.chatRoomId === String(roomId))
+      .filter((it) => it.chatRoomId === roomId)
       .sort((m1, m2) => m1.timestamp.getTime() - m2.timestamp.getTime());
   }, [messages_, roomId]);
 
